f9702662d5ba76cbb91ea86d1adf7c3fd07b684e
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2020-11-23 22:04:22 -0200

    Commit feito para registrar alterações no projeto:
    	- Criação do menu do programa.

---
+++ src/numeroaleatorio/Menu.java
@@ -0,0 +1,53 @@
+
+
+
+    // [ " CRIADOR: LUIZ ÂNGELO MARTINS " ]:
+
+
+
+// PACOTE DA CLASSE:
+package numeroaleatorio;
+
+
+// IMPORTAÇÃO DE BIBLIOTECA:
+import classes.Numero;
+import java.util.Scanner;
+
+
+// MÉTODO PRINCIPAL:
+public class Menu {
+    
+    
+    // DECLARAÇÃO DE VARIAVEIS E OBJETOS:
+    int valor1 = 0;
+    int valor2 = 0;
+    Numero gerador = new Numero();
+    
+    
+    // FUNÇÃO PARA EXIBIÇÃO DO MENU E ENTRADA DE DADOS:
+    public void exibir_menu() {
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("GERADOR DE NÚMERO ALEATÓRIO:");
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.printf("DESEJA GERAR QUANTOS NÚMEROS: ");
+        Scanner quantia_de_numeros = new Scanner(System.in);
+        valor1 = quantia_de_numeros.nextInt();
+        
+        System.out.printf("DESEJA GERAR NÚMEROS ENTRE QUAL INTERVALO ?: 1 - ");
+        Scanner intervalo_de_numeros = new Scanner(System.in);
+        valor2 = intervalo_de_numeros.nextInt();
+        
+        gerador.gerando_numero(valor1, valor2);
+        
+    }
+
+    
+}
+
+
+
+
